d7a122a2ac591abaf97bac91d6daecf3bce4364a
Bcaskey <bcaskeyu@charter.net>
2016-02-25 20:20:01 -0600

    Round 3, all but removing sibling

--- script.js
+++ script.js
@@ -10,12 +10,27 @@ function addBox() {
     var myNewBox = document.createElement('div');
     myNewBox.id=divNumber;
     myNewBox.className = 'myBox';
-    //    myNewBox.onclick=getRandomColor;
-    //    myNewBox.ondblclick=removeBox;
     myNewBox.addEventListener('click', getRandomColor);
     myNewBox.addEventListener('mouseover', mouseEnter);
     myNewBox.addEventListener('mouseleave', mouseLeave);
-//    myNewBox.addEventListner('doubleclick', removeBox);
+    myNewBox.addEventListener('dblclick', removeBox);
+        function removeBox() {
+            var x = myNewBox.id;
+//        console.log('text string blah blah  ' + x);
+
+        if (x < 1) {
+            console.log('This is Zero');
+        }
+        else if (x % 2 === 0) {
+            //console.log('This is Even')
+            //document.removeNextsibling;
+            document.body.removeChild;
+        }
+        else {
+            console.log('This is Odd')
+        }
+    }
+    
     document.body.appendChild(myNewBox);
 }
 
@@ -35,10 +50,3 @@ function getRandomColor(e) {
     }
     e.target.style.backgroundColor = color;
 }
-
-function removeBox() {
-    var parent = document.getElement("div");
-    var child = document.getElementsByClassName("myBox");
-    document.removeChild(child);
-}
-
